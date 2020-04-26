25a99af297f5887f1a3129465c2f77f2076a81ff
Kos <mr.ilyakos@gmail.com>
2020-04-27 01:06:34 +0300

    added main modules

---
+++ src/data.py
@@ -0,0 +1,116 @@
+from PIL import Image
+from ftplib import FTP
+
+# Dataset splitting into train(with validation) and test/validation sub-arrays
+# IN - dataframe : pd.DataFrame, samples per each class : int, koef : float(0 to 1) for train sub-array size.
+# OUT - dict with keys 'train' and 'not_train', values - lists of directories of samples on the ftp-server
+def data_split(df, samples_per_class=20, train_koef=0.7):
+
+    log.info('Splitting data...')
+
+    class_list = list(df['additive'].unique())
+    classes = {}
+    train_dirs, test_dirs = [], []
+
+    for item in class_list:
+        classes[item] = list(df[ df['additive']==item ]['dir'].unique())
+
+    for one_class in classes:
+        samples = np.random.permutation(classes[one_class])[:int(samples_per_class)]
+        train, test = np.split(samples, [int(samples_per_class*train_koef)])
+        train_dirs += list(train)
+        test_dirs += list(test)
+
+    log.info('Successfull splitting!')
+
+    return {'train': train_dirs, 'not_train': test_dirs}
+
+# Downloading data to server or client from ftp
+# IN - data splitted on train and not_train parts : dict,
+#      fragments count per each sample of image: int(0 to 6972),
+#      only keys of data dict to be downloaded: list
+# OUT - finish message (and data, downloaded on the client or server. path: '.../server_data/...')
+def data_downloading(splitted_data, fragments_per_sample, special_keys=[]):
+
+    log.info('Downloading train data from FTP server...')
+
+    log.info('Connecting to FTP server...')
+    try:
+        ftp = FTP()
+        ftp.connect('83.149.249.48')
+        ftp.login()
+    except Exception:
+        log.error('Connection failed!')
+        log.error(Exception)
+    try:
+        ftp.sendcmd('PORT 83,149,249,48,192,92')
+    except:
+        log.info('sendcmd PORT failed!')
+
+    log.info('Connection succeed!')
+
+    print('Connection succeed!')
+
+    log.info('Downloading data...')
+
+    for sub_array in splitted_data:
+        if len(special_keys):
+            if sub_array not in special_keys:
+                continue
+
+        path = os.getcwd() + '/server_data/' + sub_array
+        os.makedirs(path,exist_ok=True)
+
+        start = timer()
+
+        for one_dir in splitted_data[sub_array]:
+
+            ftp.cwd('/dataset/dataset_fragments/' + str(one_dir))
+
+            filenames = ftp.nlst()
+            filenames = np.random.permutation(filenames)
+
+            for filename in filenames[:fragments_per_sample]:
+                with open(path + '/' + filename, 'wb') as file:
+                    ftp.retrbinary('RETR ' + filename, file.write)
+        end = timer()
+
+        ftp.close()
+
+        print('Time: %.2f' % ( (end - start) // 60), ' m. ')
+
+    log.info('Data dowloaded successfully!')
+
+    print('Downloading completed!')
+
+# Getting data for model from images
+# IN - dataframe with image ids and class names
+# OUT - X_, Y_ and class dictionary
+def get_image_data(df):
+
+    log.info('Mining data from images...')
+
+    main_path = os.getcwd() + '/server_data'
+    sub_arrays = os.listdir(path=main_path)
+    classes = list(df['additive'].unique())
+    class_dict = {}
+    X_, Y_ = {}, {}
+
+    for index, item in enumerate(classes):
+        class_dict[item] = index
+
+    for sub_array in sub_arrays:
+        path = main_path+'/'+sub_array
+        images = os.listdir(path=path)
+        X_[sub_array] = []
+        Y_[sub_array] = []
+
+        for image in images:
+            im = list(Image.open(path+'/'+image).getdata(0))
+            class_name = class_dict[df.iloc[int(image[:-4]) - 1, ]['additive']]
+            X_[sub_array].append(im)
+            Y_[sub_array].append(class_name)
+
+    log.info('Images data received!')
+
+    return X_, Y_, class_dict
--- src/main.py
+++ src/main.py
@@ -1,16 +1,66 @@
-import sys
 import os
-# import keras
-import PIL
-import numpy
-import pandas
-# import tensorflow
+import sys
+import csv
+import numpy as np
+import pandas as pd
+import logging as log
+from timeit import default_timer as timer
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # disable GPU
+log.basicConfig(filename="/root/shared/results/my_logs.log", level=log.INFO, filemode="w")
+
+from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, ReduceLROnPlateau
+
+from .model import tcnn_compile
+from .data import data_split, data_downloading, get_image_data
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
 
 def main():
-  print('Hello world!')
-  print(os.getcwd())
-  open('/root/shared/results/hello.txt','w').write('Hello BOINC-CRYOGEL')
+
+    df = pd.read_csv('fragments-train.csv')
+    X_, Y_, class_dict = get_image_data(df)
+    Y_test = np.array(Y_['test'])
+
+
+    new_class_dict = {}
+    Yn_ = {}
+
+
+    for key in class_dict:
+        new_class_dict[class_dict[key]] = key
+
+    num_classes = len(new_class_dict)
+
+
+    # Fragments dataset
+    # for key in X_:
+    #     X_[key] = np.array(X_[key]) / 256
+    #     X_[key] = X_[key].reshape(X_[key].shape[0], 256, 256, 1)
+    #     X_[key] = np.float32(X_[key])
+    # for key in Y_:
+    #     Yn_[key] = np_utils.to_categorical(Y_[key], num_classes)
+
+    # T-CNN(2) implementation
+
+    model = tcnn_compile(conv_layers_count=2, num_classes=num_classes)
+
+    # csv_logger = CSVLogger('tcnn_crio_frags.log')
+    # early_stops = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto', baseline=None, restore_best_weights=True)
+    # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.00001)
+    # model_ckpt = ModelCheckpoint(filepath='tcnn_crio.ckpt',
+    #                          monitor='val_accuracy',
+    #                          save_best_only=True,
+    #                          verbose=1)
+
+    # model.fit(X_['train'], Yn_['train'], batch_size=64, epochs=200,
+    #                       validation_data=(X_['val'], Yn_['val']), verbose=1, shuffle=True,
+    #                       callbacks=[csv_logger, early_stops, reduce_lr, model_ckpt])
+
+    model.save("/root/shared/results/tcnn_crio_frags.h5")
 
 if __name__ == "__main__":
-  main()
-  sys.exit()
+    sys.exit(main())
