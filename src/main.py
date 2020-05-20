0e0e5f6c9a17fd6d18c36c4ad312624e6b632e99
Kos <mr.ilyakos@gmail.com>
2020-05-21 00:32:11 +0300

    test changes

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from timeit import default_timer as timer
 logging.basicConfig(filename="/root/shared/results/main.log", level=logging.INFO, filemode="w")
 log = logging.getLogger(__name__)
 
+from keras.models import load_model
 from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
 from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, ReduceLROnPlateau
 log.info('Main modules were imported')
@@ -18,8 +19,8 @@ from data import data_split, data_downloading, get_image_data
 
 log.info('Custom modules were imported')
 # fix random seed for reproducibility
-seed = 7
-np.random.seed(seed)
+# seed = 7
+# np.random.seed(seed)
 
 def main():
 
@@ -27,7 +28,7 @@ def main():
 
     log.info('Current dir ' + os.getcwd())
     try:
-        df = pd.read_csv('/src/fragments-train.csv')
+        df = pd.read_csv('/src/fragments2-lite-train.csv')
     except Exception as e:
         log.exception('Exception during reading CSV-file')
         return 1
@@ -36,12 +37,12 @@ def main():
     # READ DATAFRAME WITH TRAIN SAMPLES AND SPLIT IT BEFORE DOWNLOADING
     # AND RECEIVING IMAGE DATA
     try:
-        splitted_data = data_split(df=df, samples_per_class=5)
+        splitted_data = data_split(df=df, samples_per_class=40)
     except Exception as e:
         log.exception('Error during splitting')
         return 1
     try:
-        data_downloading(splitted_data=splitted_data, fragments_per_sample=10)
+        data_downloading(splitted_data=splitted_data, fragments_per_sample=25)
     except Exception as e:
         log.exception('Error during data downloading')
         return 1
@@ -65,8 +66,9 @@ def main():
 
         # Fragments dataset
         for key in X_:
-            X_[key] = np.array(X_[key]) / 256
+            X_[key] = np.array(X_[key], dtype=np.uint8)
             X_[key] = X_[key].reshape(X_[key].shape[0], 256, 256, 1)
+            X_[key] = X_[key] / 256
             X_[key] = np.float32(X_[key])
         for key in Y_:
             Yn_[key] = np_utils.to_categorical(Y_[key], num_classes)
@@ -76,7 +78,8 @@ def main():
     # T-CNN(2) implementation
     log.info('Data processing was finished')
     try:
-        model = tcnn_compile(conv_layers_count=2, num_classes=num_classes)
+        # model = tcnn_compile(conv_layers_count=2, num_classes=num_classes)
+        model = load_model('/src/global_tcnn2_crio_2.h5')
     except Exception as e:
         log.exception('Exception during model compiling')
         return 1
@@ -84,12 +87,12 @@ def main():
     log.info('Initializing model callbacks..')
     try:
         csv_logger = CSVLogger('/root/shared/results/tcnn_crio.log')
-        early_stops = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto', baseline=None, restore_best_weights=True)
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.00001)
-        # model_ckpt = ModelCheckpoint(filepath='/root/shared/results/tcnn_crio.ckpt',
-        #                         monitor='val_accuracy',
-        #                         save_best_only=True,
-        #                         verbose=1)
+        early_stops = EarlyStopping(monitor='val_loss', min_delta=0, patience=15, verbose=1, mode='auto', baseline=None)
+        # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=0.00001)
+        model_ckpt = ModelCheckpoint(filepath='/root/shared/results/tcnn_crio.ckpt',
+                                monitor='val_loss',
+                                save_best_only=True,
+                                verbose=1)
     except Exception as e:
         log.exception('Callbacks were not initialized')
         return 1
@@ -97,20 +100,20 @@ def main():
     log.info('Initializing completed')
     log.info('Fitting model..')
     try:
-        model.fit(X_['train'], Yn_['train'], batch_size=64, epochs=2,
+        model.fit(X_['train'], Yn_['train'], batch_size=64, epochs=10,
                             validation_data=(X_['not_train'], Yn_['not_train']), verbose=1, shuffle=True,
-                            callbacks=[csv_logger, early_stops, reduce_lr])
+                            callbacks=[csv_logger, early_stops,model_ckpt])
     except Exception as e:
         log.exception('Fitting was failed!')
         return 1
 
     log.info('Success!')
 
-    try:
-        model.save("/root/shared/results/tcnn_crio.h5")
-    except Exception as e:
-        log.exception('Exception during model saving')
-        return 1
+    # try:
+    #     model.save("/root/shared/results/tcnn_crio.h5")
+    # except Exception as e:
+    #     log.exception('Exception during model saving')
+    #     return 1
 
     log.info('Model was saved!')
 
